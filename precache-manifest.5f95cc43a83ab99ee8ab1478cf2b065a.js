50f610a7bcde52427a5fe78bd5dcf481261420e4
DMDXV <Padfootdiamond@Gmail.com>
2020-02-17 17:03:20 +0000

    Updates

---
+++ precache-manifest.5f95cc43a83ab99ee8ab1478cf2b065a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e200f0dcd91e095e429a764364bec0e6",
+    "url": "/spacex-tracker/index.html"
+  },
+  {
+    "revision": "47913865330d690cfe6e",
+    "url": "/spacex-tracker/static/css/main.5ed04ccc.chunk.css"
+  },
+  {
+    "revision": "ac308253d733659257cc",
+    "url": "/spacex-tracker/static/js/2.a25bc38a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/spacex-tracker/static/js/2.a25bc38a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47913865330d690cfe6e",
+    "url": "/spacex-tracker/static/js/main.0f0e9d98.chunk.js"
+  },
+  {
+    "revision": "e84d68b58a13d3339842",
+    "url": "/spacex-tracker/static/js/runtime-main.9905517c.js"
+  }
+]);
